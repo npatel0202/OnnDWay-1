861563b908d45c5701dade66fcc5b600e32371ed
parthvyas18896 <pvyas1@confederationcollege.ca>
2021-07-26 14:40:20 -0400

    HomeScreen

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,56 +1,191 @@
-import React from "react";
-import { render } from "react-dom";
-import { View, Text, SafeAreaView } from "react-native";
-import { Header } from "react-native-elements";
-import LinearGradient from "react-native-linear-gradient";
+// import React from "react";
+// import { render } from "react-dom";
+// import { View, Text, SafeAreaView } from "react-native";
+// import { Header } from "react-native-elements";
+// import LinearGradient from "react-native-linear-gradient";
 
-export default class HomeScreen extends React.Component {
+// export default class HomeScreen extends React.Component {
+//   render() {
+//     return (
+//       <SafeAreaView style={{ flex: 1 }}>
+//         <View style={{ flex: 1, padding: 16 }}>
+//           <Header
+//             placement="left"
+//             leftComponent={{ icon: "menu", color: "#fff" }}
+//             centerComponent={{ text: "OnnDWay", style: { color: "#fff" } }}
+//             rightComponent={{ icon: "home", color: "#fff" }}
+//           />
+
+//           <View
+//             style={{
+//               flex: 1,
+//               alignItems: "center",
+//               justifyContent: "center",
+//             }}
+//           >
+//             <Text
+//               style={{
+//                 fontSize: 20,
+//                 textAlign: "center",
+//                 marginBottom: 16,
+//               }}
+//             >
+//               This is the Home Screen
+//             </Text>
+//           </View>
+//           <Text
+//             style={{
+//               fontSize: 18,
+//               textAlign: "center",
+//               color: "grey",
+//             }}
+//           ></Text>
+//           <Text
+//             style={{
+//               fontSize: 16,
+//               textAlign: "center",
+//               color: "grey",
+//             }}
+//           ></Text>
+//         </View>
+//       </SafeAreaView>
+//     );
+//   }
+// }
+
+// //export default HomeScreen;
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  Alert,
+  ScrollView,
+  FlatList,
+} from 'react-native';
+
+export default class HomeScreen extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {id:1, title: "Home", image:"https://img.icons8.com/color/70/000000/cottage.png"},
+        {id:1, title: "Profile", image:"https://img.icons8.com/color/70/000000/administrator-male.png"},
+        {id:2, title: "Add Drivers", image:"https://img.icons8.com/color/70/000000/filled-like.png"} ,
+        {id:3, title: "List of Drivers", image:"https://img.icons8.com/color/70/000000/facebook-like.png"} ,
+        {id:4, title: "Orders", image:"https://img.icons8.com/color/70/000000/shutdown.png"} ,
+        {id:5, title: "Telephone Order", image:"https://img.icons8.com/color/70/000000/traffic-jam.png"} ,
+        {id:6, title: "History Orders", image:"https://img.icons8.com/dusk/70/000000/visual-game-boy.png"} ,
+        {id:8, title: "Cancel", image:"https://img.icons8.com/flat_round/70/000000/cow.png"} ,
+        {id:9, title: "Option 9", image:"https://img.icons8.com/color/70/000000/coworking.png"} ,
+        {id:9, title: "Option 10",image:"https://img.icons8.com/nolan/70/000000/job.png"} ,
+      ]
+    };
+  }
+
+  clickEventListener(item) {
+    Alert.alert(item.title)
+  }
+
   render() {
     return (
-      <SafeAreaView style={{ flex: 1 }}>
-        <View style={{ flex: 1, padding: 16 }}>
-          <Header
-            placement="left"
-            leftComponent={{ icon: "menu", color: "#fff" }}
-            centerComponent={{ text: "OnnDWay", style: { color: "#fff" } }}
-            rightComponent={{ icon: "home", color: "#fff" }}
-          />
+      <View style={styles.container}>
+        <FlatList style={styles.list}
+          contentContainerStyle={styles.listContainer}
+          data={this.state.data}
+          horizontal={false}
+          numColumns={2}
+          keyExtractor= {(item) => {
+            return item.id;
+          }}
+          renderItem={({item}) => {
+            return (
+              <View>
+                <TouchableOpacity style={styles.card} onPress={() => {this.clickEventListener(item)}}>
+                  <Image style={styles.cardImage} source={{uri:item.image}}/>
+                </TouchableOpacity>
 
-          <View
-            style={{
-              flex: 1,
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <Text
-              style={{
-                fontSize: 20,
-                textAlign: "center",
-                marginBottom: 16,
-              }}
-            >
-              This is the Home Screen
-            </Text>
-          </View>
-          <Text
-            style={{
-              fontSize: 18,
-              textAlign: "center",
-              color: "grey",
-            }}
-          ></Text>
-          <Text
-            style={{
-              fontSize: 16,
-              textAlign: "center",
-              color: "grey",
-            }}
-          ></Text>
-        </View>
-      </SafeAreaView>
+                <View style={styles.cardHeader}>
+                  <View style={{alignItems:"center", justifyContent:"center"}}>
+                    <Text style={styles.title}>{item.title}</Text>
+                  </View>
+                </View>
+              </View>
+            )
+          }}/>
+      </View>
     );
   }
 }
 
-//export default HomeScreen;
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    marginTop:40,
+    backgroundColor:'#f6f6f6',
+  },
+  list: {
+    paddingHorizontal: 5,
+    backgroundColor:"#f6f6f6",
+  },
+  listContainer:{
+    alignItems:'center'
+  },
+  /******** card **************/
+  card:{
+    shadowColor: '#474747',
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.37,
+    shadowRadius: 7.49,
+
+    elevation: 12,
+    marginVertical: 20,
+    marginHorizontal: 40,
+    backgroundColor:"#e2e2e2",
+    //flexBasis: '42%',
+    width:120,
+    height:120,
+    borderRadius:60,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  cardHeader: {
+    paddingVertical: 17,
+    paddingHorizontal: 16,
+    borderTopLeftRadius: 1,
+    borderTopRightRadius: 1,
+    flexDirection: 'row',
+    alignItems:"center", 
+    justifyContent:"center"
+  },
+  cardContent: {
+    paddingVertical: 12.5,
+    paddingHorizontal: 16,
+  },
+  cardFooter:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 12.5,
+    paddingBottom: 25,
+    paddingHorizontal: 16,
+    borderBottomLeftRadius: 1,
+    borderBottomRightRadius: 1,
+  },
+  cardImage:{
+    height: 50,
+    width: 50,
+    alignSelf:'center'
+  },
+  title:{
+    fontSize:18,
+    flex:1,
+    alignSelf:'center',
+    color:"#696969"
+  },
+});    
